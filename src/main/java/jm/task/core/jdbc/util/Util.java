826dad34fedf4a1420a383d74ea1560c3709bba8
Vyacheslav Polinchuk <slava.polinchuk@yandex.ru>
2025-11-13 15:16:41 +0400

    Решение задачи 1.1.4 исправления

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -2,13 +2,29 @@ package jm.task.core.jdbc.util;
 
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Util {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USER = "postgres";
+    private static final String PASS = "polavkraftking";
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to get DB connection", e);
+        }
+    }
 
     private static SessionFactory sessionFactory;
 
@@ -32,7 +48,7 @@ public class Util {
                 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
                         .applySettings(configuration.getProperties()).build();
                 sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
+            } catch (HibernateException e) {
                 e.printStackTrace();
                 throw new RuntimeException("Ошибка при инициализации SessionFactory!");
             }
